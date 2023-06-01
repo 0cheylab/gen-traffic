19fe9ffb0d5210165331fd7e9621e84f68612912
ruheez <106542889+ruheez@users.noreply.github.com>
2023-06-01 20:00:32 +0700

    Create Op.py

---
+++ Op.py
@@ -0,0 +1,34 @@
+import time
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# กำหนด URL เป้าหมาย
+target_url = "https://example.com"
+
+# กำหนดเบราว์เซอร์ที่ต้องการใช้ (ต้องมี WebDriver ของเบราว์เซอร์นั้น ๆ ลงในระบบก่อน)
+browser = webdriver.Chrome()
+
+# เข้าสู่หน้าเว็บเป้าหมาย
+browser.get(target_url)
+
+# ฟังก์ชันสำหรับการสุ่มคลิกลิงก์ในหน้าเว็บ
+def click_random_link():
+    # ค้นหาลิงก์ทั้งหมดในหน้าเว็บ
+    links = browser.find_elements(By.TAG_NAME, "a")
+    
+    # สุ่มเลือกลิงก์
+    random_link = random.choice(links)
+    
+    # คลิกลิงก์
+    random_link.click()
+
+# ลูปการคลิกลิงก์ในหน้าเว็บ
+while True:
+    # สุ่มคลิกลิงก์ในหน้าเว็บ
+    click_random_link()
+    
+    # หน่วงเวลาก่อนการคลิกลิงก์ถัดไป
+    time.sleep(random.randint(2, 5))
